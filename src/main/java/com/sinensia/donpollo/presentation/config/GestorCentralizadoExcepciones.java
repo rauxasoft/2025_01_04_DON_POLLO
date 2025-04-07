49ca637a09782819276301deaf241e9f6c8a9392
Jordi <jordi2a@gmail.com>
2025-04-07 08:51:34 +0200

    todo listo para lunes 07

--- src/main/java/com/sinensia/donpollo/presentation/config/GestorCentralizadoExcepciones.java
+++ src/main/java/com/sinensia/donpollo/presentation/config/GestorCentralizadoExcepciones.java
@@ -1,8 +1,13 @@
 package com.sinensia.donpollo.presentation.config;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.sinensia.donpollo.business.config.BusinessException;
@@ -25,5 +30,12 @@ public class GestorCentralizadoExcepciones extends ResponseEntityExceptionHandle
 		
 		return ResponseEntity.badRequest().body(new ErrorResponse(ex.getMessage()));
 	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		return handleExceptionInternal(ex, "PROBLEMOOOON", headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
 
 }
