34bec976e7a28e79e5575281638927db85a733a3
Jordi Alemany <jordia4@gmail.com>
2025-04-10 08:38:22 +0200

    nuevo endpoint delete producto

--- src/main/java/com/sinensia/donpollo/presentation/controllers/ProductoController.java
+++ src/main/java/com/sinensia/donpollo/presentation/controllers/ProductoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,6 +75,12 @@ public class ProductoController {
 		producto.setId(id);
 		productoServices.update(producto);
 	 
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		productoServices.delete(id);
 	}
 
 }
