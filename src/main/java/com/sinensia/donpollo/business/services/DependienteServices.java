a00b97c90898d72953d549c7b38cba37759f0325
Jordi <jordi2a@gmail.com>
2025-04-25 12:05:38 +0200

    locuras

--- src/main/java/com/sinensia/donpollo/business/services/DependienteServices.java
+++ src/main/java/com/sinensia/donpollo/business/services/DependienteServices.java
@@ -6,5 +6,18 @@ import com.sinensia.donpollo.business.model.Dependiente;
 
 public interface DependienteServices {
 
-	List<Dependiente> getAll();
+	List<Dependiente> getAll();y+t
+	th
+	trh
+	thr
+	ht
+	ht
+	çthr
+	çst`´çphkmrophjporthk`tgh
+	rsth
+	ht
+	rhsy
+	htr
+	
+	
 }
