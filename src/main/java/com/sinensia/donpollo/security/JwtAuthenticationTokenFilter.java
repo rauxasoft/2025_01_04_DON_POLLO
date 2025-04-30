b5913f37153c7192118bdc4709083801009c3c4b
Jordi <jordi2a@gmail.com>
2025-04-30 12:27:24 +0200

    arreglado el problema del doblre filtro

--- src/main/java/com/sinensia/donpollo/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/sinensia/donpollo/security/JwtAuthenticationTokenFilter.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,7 +16,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
 	@Autowired
@@ -29,8 +27,6 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 	
-		System.out.println("** FILTRO SEGURIDAD ************** " + request.getMethod() + request.getRequestURI() + " **************************************");    
-		
 		try {
 			
 			String token = extractToken(request);
