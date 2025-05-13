9203e647e523fc910f73ee93fbd3785ee97b843e
jordi from PC <jordia@gmail.com>
2025-05-13 16:12:11 +0200

    crossorigin en controlador de autenticacion

--- src/main/java/com/sinensia/donpollo/security/presentation/AuthController.java
+++ src/main/java/com/sinensia/donpollo/security/presentation/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import com.sinensia.donpollo.security.UserDetailsImpl;
 
 @RestController
 @RequestMapping("donpollo/auth")
+@CrossOrigin
 public class AuthController {
 
 	private Logger logger = LoggerFactory.getLogger(AuthController.class);
