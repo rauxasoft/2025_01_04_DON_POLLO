cb55669ddfbb159a7a437779750773592dbdf738
jordi from virtual machine <jordi@gmail.com>
2025-05-20 13:28:39 +0200

    se abren algunos end-points

--- src/main/java/com/sinensia/donpollo/security/SecurityConfig.java
+++ src/main/java/com/sinensia/donpollo/security/SecurityConfig.java
@@ -62,8 +62,12 @@ public class SecurityConfig {
 				    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()						// Permitir acceso a métodos OPTIONS (lo utilizan los navegadores)
 				    .requestMatchers("/h2-console/**").permitAll()								// Para permitir el acceso a la consola H2
 				    .requestMatchers("/familias/**").permitAll()								// Para permitir el acceso "/familias"
+				    .requestMatchers("/pedidos/**").permitAll()
+				    .requestMatchers("/productos/**").permitAll()
+				    //.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "(swagger-ui/**").permitAll()
+				    
 				    .anyRequest().authenticated()												// El resto de end-points requieren estar autenticado
-				
+				    
 				);
 		
 		http.authenticationProvider(getAuthenticationProvider());
